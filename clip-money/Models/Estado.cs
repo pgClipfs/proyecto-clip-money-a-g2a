570135a352be9fad5367f05df980bd7ae39d8616
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2020-11-07 01:05:20 -0300

    Agregadas,: CuentaVirtual Estado TipoCuentaVirtual

---
+++ clip-money/Models/Estado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace clip_money.Models
+{
+    public class Estado
+    {
+
+        private int id;
+        private string nombre;
+        private string descripcion;
+        private string ambito;
+    }
+}
