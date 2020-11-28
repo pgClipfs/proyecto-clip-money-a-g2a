5070d2eb7295e950e631983ea022de8663fcc962
Germán De Nicola <denicolagerman@gmail.com>
2020-11-28 12:30:34 -0300

    Se refina LoginController

--- clip-money/App_Start/WebApiConfig.cs
+++ clip-money/App_Start/WebApiConfig.cs
@@ -11,6 +11,7 @@ namespace clip_money
         public static void Register(HttpConfiguration config)
         {
             // Configuración y servicios de API web
+            config.EnableCors();
 
             // Rutas de API web
             config.MapHttpAttributeRoutes();
