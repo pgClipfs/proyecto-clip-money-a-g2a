9eeb2dc60ee331700be6d9b7af3f753f6b7341d8
Diego Rodriguez <diegonr1791@gmail.com>
2020-11-28 20:26:32 -0300

    Home Completo, retoques index, ruteo

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import{HttpClient, HttpClientModule} from '@angular/common/http';
-import { FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClienteComponent } from './components/cliente/cliente.component';
 import { ClienteService } from './services/cliente.service';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { IndexComponent } from './components/index/index.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 
 
@@ -13,12 +17,16 @@ import { ClienteService } from './services/cliente.service';
   declarations: [
     AppComponent,
     ClienteComponent,
+    HomeComponent,
+    NavbarComponent,
+    IndexComponent,
+    InicioComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserModule, FormsModule,ReactiveFormsModule
+    BrowserModule, FormsModule, ReactiveFormsModule
   ],
   providers: [ClienteService],
   bootstrap: [AppComponent]
